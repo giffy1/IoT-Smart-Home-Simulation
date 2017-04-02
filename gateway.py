12025c79ec7756ce97c1041ed26b26e4d524f313
giffy1 <giffy1991@gmail.com>
2017-04-02 11:04:10 -0400

    Implemented the IoT gateway. It subclasses MainServerSocket, allowing it to communicate asynchronously with several clients. When a client-server connection is initially made, the gateway assigns the client a unique ID (uuid), sends the ID to the client, and the client responds with its information, i.e. its type (sensor or smart device) and name.

--- gateway.py
+++ gateway.py
@@ -7,3 +7,50 @@ Created on Wed Mar 29 18:35:12 2017
 This class represents the IoT gateway.
 """
 
+from sensor import Sensor
+from server import MainServerSocket
+import time
+import json
+
+class Gateway(MainServerSocket):
+    """
+    The implementation of the IoT gateway, which acts as a server to which 
+    multiple nodes (sensors and smart devices) can connect as clients.
+    """
+    def __init__(self, port):
+        MainServerSocket.__init__(self, port)
+        self.devices = {}
+        
+    def register(self, uuid, _type, name):
+        """
+        Registers an IoT node (a sensor or smart device) with 
+        the gateway, specifying its type and name.
+        
+        type :  Either 'sensor' or 'smart device'
+        name :  The name of the device, e.g. 'motion' for motion sensor
+                or 'bulb' for smart light bulb.
+        
+        """
+        self.devices[uuid]= ( _type, name)
+        for uuid, device in self.devices.items():
+            print uuid, device
+        
+    def on_message_received(self, message):
+        """
+        Called when a message is received from one of the clients.
+        """
+        print message
+        message = json.loads(message)
+        print "received message with action : " + message['action']
+        if message['action'] == 'register':
+            print "Registering IoT unit..."
+            self.register(message['uuid'], message['type'], message['name'])
+        return
+        
+    def query_state(self, device_id):
+        self.send_message(device_id, json.dumps({'action' : 'query_state'}))
+        
+
+if __name__=='__main__': 
+    gateway = Gateway(8881)
+    gateway.launch()
