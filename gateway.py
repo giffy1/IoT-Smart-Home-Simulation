b6b0e69fd3e9fe9d83f971521158e6760b4b9583
giffy1 <giffy1991@gmail.com>
2017-04-02 20:56:17 -0400

    The gateway now handles sync and election messages by forwarding them to their destination. While the assignment details said that another layer could allow all nodes to communicate with each other directly, I chose to keep communication over the server node only, as this would be a reasonable constraint in an actual IoT system. The only drawback here is that communicating over the server is never faster than communicating directly to the other client., but this design choice would really depend on the topology and hardware constraints of the IoT network.

--- gateway.py
+++ gateway.py
@@ -19,7 +19,9 @@ class Gateway(MainServerSocket):
     """
     def __init__(self, port):
         MainServerSocket.__init__(self, port)
+        self.backend_id = 0 # assume backend is first to connect
         self.devices = {}
+        self.leader_id = -1
         
     def register(self, uuid, _type, name):
         """
@@ -34,18 +36,44 @@ class Gateway(MainServerSocket):
         self.devices[uuid]= ( _type, name)
         for uuid, device in self.devices.items():
             print uuid, device
+            
+    def send_to_backend(self, message):
+        """
+        Sends the message to the backend tier.
+        """
+        self.send_message(self.backend_id, message)
+        
         
     def on_message_received(self, message):
         """
         Called when a message is received from one of the clients.
         """
         print message
+#        self.send_to_backend(message)
         message = json.loads(message)
         print "received message with action : " + message['action']
         if message['action'] == 'register':
             print "Registering IoT unit..."
             self.register(message['uuid'], message['type'], message['name'])
-        return
+        elif message['action'] == 'election':
+            for i in range(int(message['uuid']) + 1, self.uuid):
+                print "sending to " + str(i)
+                self.send_message(i, json.dumps({'action' : 'election', 'called_by' : message['uuid']}))
+        elif message['action'] == 'election_response':
+            self.send_message(int(message['respond_to']), json.dumps(message)) # forward to other clients
+        elif message['action'] == 'election_won':
+            print str(message['uuid']) + " won the election."
+            self.leader_id = int(message['uuid'])
+            for i in range(1, self.uuid):
+                self.send_message(i, json.dumps(message)) # forward to all participants
+        elif message['action'] == 'sync':
+            for i in range(1, self.uuid):
+                if i != message['uuid']: # ignore master
+                    self.send_message(i, json.dumps(message)) # forward to slaves
+        elif message['action'] == 'sync_response':
+            self.send_message(message['respond_to'], json.dumps(message)) # forward response to master
+        elif message['action'] == 'sync_complete':
+            self.send_message(message['send_to'], json.dumps(message)) # forward to slaves
         
     def query_state(self, device_id):
         self.send_message(device_id, json.dumps({'action' : 'query_state'}))
