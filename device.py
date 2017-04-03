2ea1f99e0129a7363a683522f9605ecfd77cb8ed
giffy1 <giffy1991@gmail.com>
2017-04-02 20:52:29 -0400

    Implemented the bully leader election algorithm and the Berkeley clock synchronization algorithm. When a device is connected to the server, it initiates an election, and the client with the highest ID is then determined to be the leader. The leader then initiates a sync request, which is sent to all other clients. Each client replies with its timestamp and the time of flight is used to estimate the difference in time. The adjusted times are then averaged over all clients and the offset is used to correct each device's timestamp.

--- device.py
+++ device.py
@@ -8,35 +8,68 @@ Base class for an IoT device, either a sensor or a smart device.
 """
 
 from client import Client
+import time
 
 class Device(Client):
-    def __init__(self, address):
+    def __init__(self, address, _type, name):
         """
         address : the address of the gateway to which the device should connect.
         """
         Client.__init__(self, address)
+        self._type = _type
+        self.name = name
+        self.state = 0
         self.uuid = -1
+        self.election_lost = False
+        self.leader_id = -1
+        self.time_estimates = {}
+        self.t0 = -1
+        self.offset = 0
     
     def register(self):
         """
         Registers the device with the IoT gateway.
         """
-        self.send_message({'uuid' : self.uuid, 'action' : 'register', 'type' : 'device', 'name' : 'bulb'})
+        self.send_message({'uuid' : self.uuid, 'action' : 'register', 'type' : self._type, 'name' : self.name})
         return
         
     def report_state(self):
         """
-        Method stub for reporting the state of the device. This should be implemented by concrete subclasses.
+        Reports the sensor state to the gateway.
         """
-        # should be overridden in subclass
+        self.send_message({'uuid' : self.uuid, 'action' : 'report_state', 'state' : self.state, 'timestamp' : time.time() - self.offset})
+        
+    def change_state(self, state):
+        """
+        Sets the state of the device. TODO: If _type is 'sensor', throw exception
+        """
+        self.state = state
+        self.report_state()
         return
         
-    def change_state(self):
-        """
-        Method stub for changing the state of the device. This should be implemented by concrete subclasses.
-        """
-        # should be overridden in subclass
-        return
+    def start_election(self):
+        self.send_message({'uuid' : self.uuid, 'action' : 'election'})
+        self.election_lost = False
+        time.sleep(0.5)
+        if not self.election_lost:
+            self.send_message({'uuid' : self.uuid, 'action' : 'election_won'})
+            self.election_won()
+            
+    def election_won(self):
+        self.time_estimates = {}
+        self.send_message({'uuid' : self.uuid, 'action' : 'sync'}) # master in Berkeley sync algorithm
+        self.t0 = time.time()
+        time.sleep(0.5) # wait for all responses (those that take too long can be ignored)
+        tf = time.time()
+        keys = self.time_estimates.keys()
+        print "received sync responses from " + str(len(keys)) + " nodes."
+        avg_time = 0
+        for k in keys:
+            avg_time += self.time_estimates[k]
+        avg_time += self.t0 + (tf - self.t0) / 2 # include self
+        avg_time = avg_time / (len(keys) + 1)
+        for k in keys:
+            self.send_message({'uuid' : self.uuid, 'send_to' : k, 'action' : 'sync_complete', 'offset' : (self.time_estimates[k] - avg_time)})
         
     def on_message_received(self, message):
         """
@@ -46,14 +79,47 @@ class Device(Client):
             self.report_state()
         elif message['action'] == 'register':
             self.uuid = message['uuid']
-            print "my UUID is " + str(self.uuid)
+            print "UUID of " + self.name + " is " + str(self.uuid) + ". Calling election."
             self.register()
+#            self.start_election()
         elif message['action'] == 'change_state':
-            self.change_state()
+            self.change_state(message['state'])
+        elif message['action'] == 'election':
+            print self.name + " received notification that an election was called by " + str(message['called_by']) + ". Calling new election."
+            self.send_message({'uuid' : self.uuid, 'action' : 'election_response', 'respond_to' : message['called_by']})
+            self.start_election()
+        elif message['action'] == 'election_response':
+            print self.name + " has lost the election."
+            self.election_lost = True
+        elif message['action'] == 'election_won':
+            print self.name + " received notification that the election was won by " + str(message['uuid'])
+            self.leader_id = int(message['uuid'])
+        elif message['action'] == 'sync':
+            self.send_message({'uuid' : self.uuid, 'action' : 'sync_response', 'timestamp' : time.time(), 'respond_to' : message['uuid']})
+        elif message['action'] == 'sync_response':
+            t_reply = (time.time() - self.t0) / 2
+            print "Received a reply from " + str(message['uuid']) + " with TOF " + str(t_reply)
+            self.time_estimates[message['uuid']] = message['timestamp'] + t_reply
+        elif message['action'] == 'sync_complete':
+            self.offset = message['offset']
+            print self.name + " has an offset of " + str(self.offset)
             
             
         
 if __name__=='__main__': 
     address = ('localhost', 8881)
-    device = Device(address)
-    device.connect()
+    temperature_sensor = Device(address, 'sensor', 'temperature')
+    temperature_sensor.connect()
+    
+    time.sleep(0.5)
+    
+    motion_sensor = Device(address, 'sensor', 'motion')
+    motion_sensor.connect()
+    
+    time.sleep(0.5)
+    
+#    motion_sensor.disconnect()
+#    
+#    time.sleep(0.5)
+    
+    temperature_sensor.start_election()
